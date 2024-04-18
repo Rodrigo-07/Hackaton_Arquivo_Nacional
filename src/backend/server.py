2024c60f304be598c1da4d144a4797189866c35f
Caio <caio.paula@sou.inteli.edu.br>
2024-04-18 16:08:02 -0300

    feat : adiciona tailwind ao frontend

--- src/backend/server.py
+++ src/backend/server.py
@@ -3,3 +3,7 @@ from fastapi import FastAPI
 server = FastAPI()
 
 
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(server)
---
+++ src/frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/frontend/src/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

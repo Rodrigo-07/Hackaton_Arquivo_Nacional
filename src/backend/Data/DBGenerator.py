ee315e37e02a1b282a08ff97d68dfce5606b438d
mauroDasChagas <mauro.junior@sou.inteli.edu.br>
2024-04-19 02:24:55 -0300

    add: base de dados das tags para o rogério

--- src/backend/Data/DBGenerator.py
+++ src/backend/Data/DBGenerator.py
@@ -1,6 +1,7 @@
 import os
-from importlib.machinery import SourceFileLoader
 import sqlite3
+from importlib.machinery import SourceFileLoader
+from typing import get_type_hints
 
 conn = sqlite3.connect('Data/database/db.sqlite')
 cursor = conn.cursor()
@@ -22,8 +23,9 @@ for filename in os.listdir(models_folder):
 
         # verifica se o modelo e o nome da tabela foram encontrados
         if table_name and model:
-            # monta a consulta 
-            columns = ', '.join([f"{field.name} {field.type_.__name__}" for field in model.__fields__.values()])
+            # inspeciona os tipos para obter informações sobre os campos
+            field_types = get_type_hints(model)
+            columns = ', '.join([f"{field_name} {field_type.__name__}" for field_name, field_type in field_types.items()])
             create_table_sql = f"CREATE TABLE IF NOT EXISTS {table_name} ({columns})"
 
             cursor.execute(create_table_sql) # executa consulta e cria tabela
--- src/backend/main.py
+++ src/backend/main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
-from fastapi.openapi.utils import get_openapi
-from fastapi.openapi.docs import get_swagger_ui_html
-from starlette.responses import JSONResponse
 from Controllers.UserController import router as user_router
 from Controllers.OutputController import router as output_router
 from Controllers.DocumentController import router as document_router
+from Controllers.TagController import router as tag_router
 
 app = FastAPI()
 
@@ -12,3 +10,4 @@ app = FastAPI()
 app.include_router(user_router)
 app.include_router(output_router)
 app.include_router(document_router)
+app.include_router(tag_router)

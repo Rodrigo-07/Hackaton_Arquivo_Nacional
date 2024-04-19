b15128e5d5dcc57ffd7b7409b89c6a9ac50be002
ItsVasconcellos <fernando.asv@uol.com.br>
2024-04-19 20:33:51 -0300

    feat: adding comments to page

---
+++ src/frontend/src/mock/comments.jsx
@@ -0,0 +1,56 @@
+export const title_comments = [
+    {
+        id_user: 2,
+        title: "Another title",
+        date: "2022-09-01",
+        upvotes_normal: 10,
+        downvote: 5,
+        justify: "Subtitle for Another title",
+    },
+    {
+        id_user: 3,
+        title: "Yet another title",
+        date: "2022-09-02",
+        upvotes_normal: 15,
+        downvote: 3,
+        justify: "Subtitle for Yet another title",
+    },
+];
+
+export const data_comments = [
+    {
+        id_user: 2,
+        title: "Another title",
+        date: "2022-09-01",
+        upvotes_normal: 10,
+        downvote: 5,
+        justify: "Subtitle for Another title",
+    },
+    {
+        id_user: 3,
+        title: "Yet another title",
+        date: "2022-09-02",
+        upvotes_normal: 15,
+        downvote: 3,
+        justify: "Subtitle for Yet another title",
+    },
+];
+
+export const context_comments = [
+    {
+        id_user: 4,
+        title: "New title",
+        date: "2022-09-03",
+        upvotes_normal: 8,
+        downvote: 2,
+        justify: "Subtitle for New title",
+    },
+    {
+        id_user: 5,
+        title: "Another new title",
+        date: "2022-09-04",
+        upvotes_normal: 12,
+        downvote: 6,
+        justify: "Subtitle for Another new title",
+    },
+];

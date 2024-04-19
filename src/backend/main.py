f74354f2bd4ed2607f626d781b73188b71651af8
mauroDasChagas <mauro.junior@sou.inteli.edu.br>
2024-04-19 02:41:12 -0300

    corrigindo cors

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from Controllers.UserController import router as user_router
 from Controllers.OutputController import router as output_router
 from Controllers.DocumentController import router as document_router
 from Controllers.TagController import router as tag_router
 
 app = FastAPI()
+
+# cors
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # roteadores
 app.include_router(user_router)

295fac039461f5983d8743cc940b7401af007c63
mauroDasChagas <mauro.junior@sou.inteli.edu.br>
2024-04-19 21:13:06 -0300

    ⚡️ add: data mock forte

--- src/frontend/src/mock/comments.jsx
+++ src/frontend/src/mock/comments.jsx
@@ -1,56 +1,56 @@
 export const title_comments = [
     {
         id_user: 2,
-        title: "Another title",
+        title: "Dois homens do século XIX conversando",
         date: "2022-09-01",
-        upvotes_normal: 10,
+        upvotes_normal: 20,
         downvote: 5,
-        justify: "Subtitle for Another title",
+        justify: "A camêra fotográfica foi criada no século XIX, logo a foto só pode ter sido criada a partir desse período",
     },
     {
         id_user: 3,
-        title: "Yet another title",
+        title: "Dois homens da década de 70 conversando sobre algo",
         date: "2022-09-02",
-        upvotes_normal: 15,
-        downvote: 3,
-        justify: "Subtitle for Yet another title",
+        upvotes_normal: 2,
+        downvote: 9,
+        justify: "A foto parece ser antiga, mas não sei identificar o período correto, vou deixar década de 70 por via das dúvidas",
     },
 ];
 
 export const data_comments = [
     {
         id_user: 2,
-        title: "Another title",
+        title: "Século XIX",
         date: "2022-09-01",
-        upvotes_normal: 10,
+        upvotes_normal: 30,
         downvote: 5,
-        justify: "Subtitle for Another title",
+        justify: "A camêra fotográfica foi criada no século XIX, logo a foto só pode ter sido criada a partir desse período",
     },
     {
         id_user: 3,
-        title: "Yet another title",
+        title: "Década de 70",
         date: "2022-09-02",
-        upvotes_normal: 15,
-        downvote: 3,
-        justify: "Subtitle for Yet another title",
+        upvotes_normal: 9,
+        downvote: 7,
+        justify: "A foto parece ser antiga",
     },
 ];
 
 export const context_comments = [
     {
         id_user: 4,
-        title: "New title",
+        title: "Os dois homens estão conversando sobre as crises políticas de sua época",
         date: "2022-09-03",
         upvotes_normal: 8,
         downvote: 2,
-        justify: "Subtitle for New title",
+        justify: "Os dois homens parecem ser políticos pelas suas vestimentas, faria sentido essa ser sua atividade.",
     },
     {
         id_user: 5,
-        title: "Another new title",
+        title: "Os dois homens estão discutindo sobre negócios e economia",
         date: "2022-09-04",
         upvotes_normal: 12,
         downvote: 6,
-        justify: "Subtitle for Another new title",
+        justify: "Pelo ambiente aonde estão, pode-se concluir que os dois homens são grandes empresários de sua época e estão discutindo sobre.",
     },
 ];
